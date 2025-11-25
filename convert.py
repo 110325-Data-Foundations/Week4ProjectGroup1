963e71e1f4d9fe105022a859dafce7803473c86e
David Barboza <davidb221@gmail.com>
2025-11-25 13:38:55 -0600

    imported cfb data into csv

--- convert.py
+++ convert.py
@@ -8,5 +8,5 @@ database_url = os.getenv('DATABASE_URL')
 
 engine = create_engine(database_url)
 
-games_df = pd.read_sql_table('games', engine)
-games_df.to_csv("games.csv", index=False)
+cfbdf = pd.read_sql_query('SELECT g.*,ht.id as home_team_id,ht.name_full as home_team_full_name,ht.name_school as home_team_school_name, ht.mascot as home_team_mascot, at.id as away_team_id, at.name_full as away_team_full_name, at.name_school as away_team_school_name, at.mascot as away_team_mascot FROM games g JOIN teams ht ON ht.id = g.id_home_team JOIN teams at ON at.id = g.id_away_team;', engine)
+cfbdf.to_csv("./data/cfb_data.csv", index=False)
