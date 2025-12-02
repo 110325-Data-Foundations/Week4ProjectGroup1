5620281a0bd0e1dd111e3e1996d57e3c618202fd
TylerP2405 <thinhpham2405@gmail.com>
2025-12-02 13:49:52 -0600

    Fix validation logic

--- processing/validation.py
+++ processing/validation.py
@@ -4,6 +4,9 @@ def split_valid_invalid(df):
     copiedDF = df.copy()
     copiedDF['home_team_ranking'] = copiedDF['home_team_ranking'].fillna(-1)
     copiedDF['away_team_ranking'] = copiedDF['away_team_ranking'].fillna(-1)
+    copiedDF['home_team_mascot'] = copiedDF['home_team_mascot'].fillna('N/A')
+    copiedDF['away_team_mascot'] = copiedDF['away_team_mascot'].fillna('N/A')
+
 
     null_row_mask = copiedDF.isna().any(axis=1)
     invalid_df = copiedDF[null_row_mask].copy()
@@ -13,7 +16,4 @@ def split_valid_invalid(df):
 
 def clean_valid_df(df):
     cleaned_df = df.drop_duplicates().copy()
-    cleaned_df['home_team_mascot'] = cleaned_df['home_team_mascot'].fillna('N/A')
-    cleaned_df['away_team_mascot'] = cleaned_df['away_team_mascot'].fillna('N/A')
-
     return cleaned_df
