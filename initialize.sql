88cd3f8e5124565391796e694d632f5f6f8807cb
Matter405 <matthew@doerrstop.com>
2025-12-02 14:09:10 -0600

    Fixed invalid data separation

--- initialize.sql
+++ initialize.sql
@@ -43,4 +43,7 @@ CREATE TABLE yearly_team (
         references Teams(team_id)
 );
 
-Select * from garbages limit 10
+Select * from yearly_team limit 10
+drop table games;
+drop table garbages;
+drop table teams;
